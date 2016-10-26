a6a103b1c14a93ce09aa0db756118109479d96ee
quietstorm75 <dennisgr@optonline.net>
2016-10-26 06:41:54 -0400

    hthrth

--- Screens/Resources/Resource.Designer.cs
+++ Screens/Resources/Resource.Designer.cs
@@ -63,6 +63,30 @@ namespace Screens
 			// aapt resource value: 0x7f050000
 			public const int MyButton = 2131034112;
 			
+			// aapt resource value: 0x7f050007
+			public const int btnRegister = 2131034119;
+			
+			// aapt resource value: 0x7f050005
+			public const int btnSend = 2131034117;
+			
+			// aapt resource value: 0x7f050008
+			public const int btnSignIn = 2131034120;
+			
+			// aapt resource value: 0x7f050004
+			public const int editEmail = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int editFName = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int editLName = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int txtRegister = 2131034113;
+			
+			// aapt resource value: 0x7f050006
+			public const int txtwelcome = 2131034118;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -78,6 +102,12 @@ namespace Screens
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Register = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Welcome = 2130903042;
 			
 			static Layout()
 			{
